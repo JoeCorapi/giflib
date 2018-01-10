3e2e0c02ffff32fd3d9676348b1a7a445d608ba3
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-01-10 10:54:26 -0500

    Added provided styles and content

--- src/main/java/com/ubnik/giflib/controller/GifController.java
+++ src/main/java/com/ubnik/giflib/controller/GifController.java
@@ -1,14 +1,31 @@
 package com.ubnik.giflib.controller;
 
+import com.ubnik.giflib.data.GifRepository;
+import com.ubnik.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDate;
+
 @Controller
 public class GifController {
+
+    @Autowired
+    private GifRepository gifRepository;
 
     @RequestMapping("/")
     public String listGifs(){
         return "home";
     }
+
+    @RequestMapping("gif/")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = gifRepository.findByName("android-explosion");
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
+
 }
